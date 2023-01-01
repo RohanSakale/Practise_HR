597d2ef50189a9064f6199176937bf906c4b4d64
dac <dac@dypm.org>
2023-01-01 18:18:11 +0530

    done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const path=require("path");
 app.get("/",function(req,resp){
-resp.send("Hello Welcome to IACSD");
+resp.send("Hello Welcome to IACSD,Akurdi-Pune(411044)");
 });
 app.get("/home",function(req,resp){
 resp.sendFile(path.join(__dirname,"./public/index.html"));
